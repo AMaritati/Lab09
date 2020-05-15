9ac0c9942675601c3da8e1634fafa7633ef49066
Administrator <Administrator@BATAR02M002.rete.poste>
2020-05-15 17:40:16 +0200

    EXERCISE 1 DONE!

--- src/main/java/it/polito/tdp/borders/model/Model.java
+++ src/main/java/it/polito/tdp/borders/model/Model.java
@@ -1,10 +1,12 @@
 package it.polito.tdp.borders.model;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graphs;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 
@@ -36,8 +38,28 @@ public class Model {
 		for(Border b : confini) {
 			this.graph.addEdge(b.getC1(), b.getC2());
 		}
+		
 	}
 	
+	/**
+	 * Metodo per studiare il grado di ogni nazione,
+	 * mi indica il numero di confini
+	 * @return
+	 */
+	public String degreesVertexes() {
+		String elenco = "";
+		for (Country c : this.graph.vertexSet()) {
+			elenco+= c.getNome()+" -- "+this.graph.degreeOf(c)+" confini\n";
+		}
+		return elenco;
+	}
+	
+
+	public int componentiConnessi() {
+		
+		ConnectivityInspector<Country,DefaultEdge> graf = new ConnectivityInspector<>(this.graph);
+		return graf.connectedSets().size();
+	}
 	
 	public int nVertici() {
 		return this.graph.vertexSet().size();
@@ -45,6 +67,14 @@ public class Model {
 	
 	public int nArchi() {
 		return this.graph.edgeSet().size();
+	}
+	
+	/**
+	 * Restituisce la collezione di nazioni
+	 * @return
+	 */
+	public Collection<Country> getCountries(){
+		return this.graph.vertexSet();
 	}
 	
 	/**
--- src/main/java/it/polito/tdp/borders/model/TestModel.java
+++ src/main/java/it/polito/tdp/borders/model/TestModel.java
@@ -5,15 +5,18 @@ public class TestModel {
 	public static void main(String[] args) {
 
 		Model model = new Model();
-
-		System.out.println("TestModel -- TODO");
 		
 		System.out.println("Creo il grafo relativo al 2000");
-		model.creaGrafo(1960);
+		model.creaGrafo(1900);
 		
         System.out.format("Trovate %d nazioni\n", model.nVertici());
-
-		System.out.format("Numero componenti connesse: %d\n", model.nArchi());
+        System.out.format("%d\n", model.getCountries().size());
+        
+		System.out.format("Numero archi: %d\n", model.nArchi());
+		
+		System.out.format("Numero componenti connesse: %d\n", model.componentiConnessi());
+		
+		System.out.println(model.degreesVertexes());
 		
 //		Map<Country, Integer> stats = model.getCountryCounts();
 //		for (Country country : stats.keySet())
